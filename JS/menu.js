04b15bc71eece2fd44c373db218bb173a1043ab2
RomiAlmiron <romina89_@hotmail.com>
2023-09-30 20:55:47 -0300

    Cambios en nav

--- JS/menu.js
+++ JS/menu.js
@@ -1,7 +1,7 @@
-// Seleccionar el menú y los botones de abrir y cerrar
+/*// Seleccionar el menú y los botones de abrir y cerrar
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
-/*const closeMenuBtn = document.querySelector(".close-menu");*/
+/*const closeMenuBtn = document.querySelector(".close-menu");
 const menuItems = document.querySelectorAll(".menu li");
 
 // Función para alternar la clase que muestra/oculta el menú
@@ -15,21 +15,24 @@ function hideMenu() {
 }
 openMenuBtn.addEventListener("touchstart", toggleMenu);
 /*openMenuBtn.addEventListener("click", toggleMenu);*/
-/*closeMenuBtn.addEventListener("click", toggleMenu);*/
+/*closeMenuBtn.addEventListener("click", toggleMenu);
 
 // Agregar un escuchador de evento a cada elemento del menú
 menuItems.forEach(item => {
   // Oculta el menú cuando se hace clic en una opción
   item.addEventListener("touchstart", hideMenu);
-});
-/*
-// Seleccionar el menú y el botón de abrir
+});*/
+
+// Selecciona el menú y el botón de abrir
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const menuItems = document.querySelectorAll(".menu li");
 
 // Función para alternar la clase que muestra/oculta el menú
-function toggleMenu() {
+function toggleMenu(event) {
+  if(event){
+    event.preventDefault();//evita que se abra y cierre el boton del menu inmediatamente
+  }
   menu.classList.toggle("menu_opened");
 }
 
@@ -38,14 +41,35 @@ function hideMenu() {
   menu.classList.remove("menu_opened");
 }
 
-// Usar eventos "click" y "touchstart" para abrir el menú
+// Usa eventos "click" y "touchstart" para abrir el menú
 openMenuBtn.addEventListener("click", toggleMenu);
 openMenuBtn.addEventListener("touchstart", toggleMenu);
 
-// Agregar un escuchador de evento a cada elemento del menú para ocultar el menú
+// Agrega un escuchador de evento a cada elemento del menú para ocultar el menú
 menuItems.forEach(item => {
   // Oculta el menú cuando se hace clic o se toca una opción
-  item.addEventListener("click", hideMenu);
-  item.addEventListener("touchstart", hideMenu);
-});*/
+  item.addEventListener("click", function(event){
+    hideMenu();
+    navigateToSection(event.target.getAttribute("href"));//Permite navegar a la sección seleccionada del menu
+  });
+  item.addEventListener("touchstart", function(event){
+    event.preventDefault();
+    hideMenu();
+    navigateToSection(event.target.getAttribute("href"));
+  });
+});
+
+// Función para navegar a la sección
+function navigateToSection(sectionId) {
+  const section = document.querySelector(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+// Agrega un event listener al documento para cerrar el menú al tocar cualquier parte de la pantalla
+document.addEventListener("click", function (event) {
+  if (!menu.contains(event.target) && event.target !== openMenuBtn) {
+    hideMenu();
+  }
+});
 
